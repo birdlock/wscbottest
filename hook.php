22af52a40f3563607304db543b6f6f51f8c7650f
BirDz <44285092+birdlock@users.noreply.github.com>
2018-11-13 15:23:20 +0700

    Update hook.php

--- hook.php
+++ hook.php
@@ -14,7 +14,7 @@ while (($objArr = fgetcsv($objCSV, 1000, ",")) !== FALSE) {
     $row++;
  }
 fclose($objCSV);
-echo $row-1."<br>";
+echo $row."<br>";
 echo $objArr[5][0];
 if ( sizeof($request_array['events']) > 0 )
 {
