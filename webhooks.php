be28b8e95c09c5306892eba211cd041299e936f7
BirDz <44285092+birdlock@users.noreply.github.com>
2018-11-13 11:30:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -22,10 +22,10 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text,
-				'type' => 'text',
-				'text' => "hello"
+				{'type' => 'text',
+				'text' => $text;},
+				{'type' => 'text',
+				'text' => "hello"}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
