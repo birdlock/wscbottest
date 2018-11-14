e9b4136f2c83dca042e3a4a49532db8c1a5213ac
BirDz <44285092+birdlock@users.noreply.github.com>
2018-11-14 08:58:59 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
-$db = parse_url(getenv("https://data.heroku.com/datastores/fd26cf23-b905-447f-868f-82d7ae70969e#"));
-if($db){
-    echo $db;
+$conn_string = "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d2ffj87ve9jfbj user=auxvxlzecfeirx password=3246846dc64d449a9d9a6798839878869d7d72677127cd94ef10f4809d3d7fdd";
+$dbconn = pg_connect($conn_string);
+if($dbconn){
+    echo 'success';
 }
 else{
-    echo $db.'error';
+    echo $dbconn;
 }
-$db["path"] = ltrim($db["path"], "/");
 ?>
