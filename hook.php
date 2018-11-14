89e69f4f14bdb5d82ba4068c2aad979e5e71e461
BirDz <44285092+birdlock@users.noreply.github.com>
2018-11-14 09:48:15 +0700

    Update hook.php

--- hook.php
+++ hook.php
@@ -13,7 +13,7 @@ $item2=array();
 $objCSV = fopen("production.csv", "r");
 while($row = fgetcsv($objCSV)) {
   if (in_array(32, $row)) {
-      echo '<div>' . implode(' | ', $row[5]) . ' </div>';
+      echo '<div>' . implode(' | ', $row) . ' </div>';
   }
 }
 if ( sizeof($request_array['events']) > 0 )
